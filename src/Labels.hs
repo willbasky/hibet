775826e4c59eee764afae009025a72729ccf8144
silverweed <silverweed1991@gmail.com>
2019-09-26 21:52:33 +0200

    Replace JSON with TOML using tomland

--- src/Labels.hs
+++ src/Labels.hs
@@ -1,44 +1,30 @@
+{-# OPTIONS -Wno-unused-top-binds #-}
+{-# LANGUAGE LambdaCase           #-}
+{-# LANGUAGE OverloadedStrings    #-}
+
 module Labels
        ( LabelFull(..)
        , labels
        ) where
 
-import Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), decode, object, withObject, (.:), (.=))
 import Data.List (sortOn)
 import Data.Text (Text)
+import Toml (TomlCodec, (.=))
 
 import Paths_Hibet (getDataFileName)
 
-import qualified Data.ByteString.Lazy.Char8 as BLC
-import qualified Data.HashMap.Strict as HM
+import qualified Data.Text.IO as TIO
+import qualified Toml
 
 
 labels :: IO [LabelFull]
 labels = do
-    file <- getDataFileName "stuff/titles.json"
-    meta <- BLC.readFile file
-    case decode meta :: Maybe Labels of
-        Nothing               -> error "Not decoded"
-        Just (Labels decoded) -> pure $ sortOn lfId decoded
+    file <- getDataFileName "stuff/titles.toml"
+    meta <- TIO.readFile file
+    case Toml.decode labelsCodec meta of
+        Left err               -> error $ show err
+        Right (Labels decoded) -> pure $ sortOn lfId decoded
 
-data Label = Label
-    { labelId    :: Int
-    , labelLabel :: Text
-    , labelMeta  :: Text
-    } deriving (Eq, Show, Ord)
-
-instance ToJSON Label where
-    toJSON Label{..} = object
-        [ "id"     .= labelId
-        , "label"  .= labelLabel
-        , "about"  .= labelMeta
-        ]
-
-instance FromJSON Label where
-    parseJSON = withObject "label" $ \v -> Label
-        <$> v .: "id"
-        <*> v .: "label"
-        <*> v .: "about"
 
 data LabelFull = LabelFull
     { lfPath  :: Text
@@ -47,12 +33,19 @@ data LabelFull = LabelFull
     , lfMeta  :: Text
     } deriving (Eq, Show, Ord)
 
-newtype Labels = Labels [LabelFull]
-    deriving (Eq, Show)
 
-instance FromJSON Labels where
-    parseJSON v = Labels . map toLabelFull . HM.toList <$> parseJSON v
-      where
-        toLabelFull (path, Label number label about) = LabelFull path number label about
+data Labels = Labels
+    { labelTitles :: [LabelFull]
+    } deriving (Eq, Show)
+
+labelFullCodec :: TomlCodec LabelFull
+labelFullCodec = LabelFull
+    <$> Toml.text "path"  .= lfPath
+    <*> Toml.int  "id"    .= lfId
+    <*> Toml.text "label" .= lfLabel
+    <*> Toml.text "about" .= lfMeta
 
 
+labelsCodec :: TomlCodec Labels
+labelsCodec = Labels
+    <$> Toml.list labelFullCodec "titles" .= labelTitles
