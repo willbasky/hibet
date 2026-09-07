b8a3e81145a1d173261fd13301a9cfb992fdf013
Vladislav Sabanov <email@email.com>
2026-09-07 12:01:33 +0500

    Add typed tokens

--- hewts/src/Convert/Token.hs
+++ hewts/src/Convert/Token.hs
@@ -4,9 +4,8 @@ import Data.Char (chr, isHexDigit, ord)
 import Data.Text (Text)
 import Numeric (readHex)
 import Text.Printf (printf)
--- import Language (Consonant(CT))
 
--- | Превращает строку в формат "\x0f40\x0fad..."
+-- | Превращает строку в формат "\\x0f40\\x0fad..."
 toUnicodeEscape :: String -> String
 toUnicodeEscape = concatMap charToHex
     where
@@ -69,10 +68,24 @@ data Span = Span
     }
     deriving (Show, Eq)
 
+-- Canonical typed payload of an IR token.
+data TokenCanonical
+    = TcConsonant Consonant
+    | TcSubConsonant SubConsonant
+    | TcVowel Vowel
+    | TcFinal FinalMark
+    | TcNumber Number
+    | TcHalfNumber HalfNumber
+    | TcPunctuation PunctuationMark
+    | TcSpace SpaceMark
+    | TcSymbol SymbolMark
+    | TcUnknown UnknownMark
+    deriving (Show, Eq)
+
 -- Canonical IR token used as contract between tokenizer and grammar layers.
 data Token = Token
     { tokenRaw :: Text
-    , tokenCanonical :: Text
+    , tokenCanonical :: TokenCanonical
     , tokenKind :: TokenKind
     , tokenSource :: TokenSource
     , tokenSpan :: Span
@@ -88,96 +101,189 @@ data Token = Token
 -- "ོུ"
 
 data Consonant
-    = Ck -- ཀ \u0f40 
-    | Ckh -- ཁ \u0f41 
-    | Cg -- ག \u0f42 
-    | CgPLUSh -- གྷ \u0f43 
-    | Cng -- ང \u0f44 
-    | Cc -- ཅ \u0f45 
-    | Cch -- ཆ \u0f46 
-    | Cj -- ཇ \u0f47 
-    | Cny -- ཉ \u0f49 
-    | CT -- ཊ \u0f4a 
-    | CTh -- ཋ \u0f4b 
-    | CD -- ཌ \u0f4c 
-    | CDPLUSh -- ཌྷ \u0f4d 
-    | CN -- ཎ \u0f4e 
-    | Ct -- ཏ \u0f4f 
-    | Cth -- ཐ \u0f50 
-    | Cd -- ད \u0f51 
-    | CdPLUSh -- དྷ \u0f52 
-    | Cn -- ན \u0f53 
-    | Cp -- པ \u0f54 
-    | Cph -- ཕ \u0f55 
-    | Cb -- བ \u0f56 
-    | CbPLUSh -- བྷ \u0f57 
-    | Cm -- མ \u0f58 
-    | Cts -- ཙ \u0f59 
-    | Ctsh -- ཚ \u0f5a 
-    | Cdz -- ཛ \u0f5b 
-    | CdzPLUSh -- ཛྷ \u0f5c 
-    | Cw -- ཝ \u0f5d 
-    | Czh -- ཞ \u0f5e 
-    | Cz -- ཟ \u0f5f 
-    | C' -- འ \u0f60 
-    | Cy -- ཡ \u0f61 
-    | Cr -- ར \u0f62 
-    | Cl -- ལ \u0f63 
-    | Csh -- ཤ \u0f64 
-    | CSh -- ཥ \u0f65 
-    | Cs -- ས \u0f66 
-    | Ch -- ཧ \u0f67 
-    | Ca -- ཨ \u0f68 
-    | CkPLUSSh -- ཀྵ \u0f69 
-    | CR -- ཪ \u0f6a 
+    = Ck -- ཀ \u0f40
+    | Ckh -- ཁ \u0f41
+    | Cg -- ག \u0f42
+    | CgPLUSh -- གྷ \u0f43
+    | Cng -- ང \u0f44
+    | Cc -- ཅ \u0f45
+    | Cch -- ཆ \u0f46
+    | Cj -- ཇ \u0f47
+    | Cny -- ཉ \u0f49
+    | CT -- ཊ \u0f4a
+    | CTh -- ཋ \u0f4b
+    | CD -- ཌ \u0f4c
+    | CDPLUSh -- ཌྷ \u0f4d
+    | CN -- ཎ \u0f4e
+    | Ct -- ཏ \u0f4f
+    | Cth -- ཐ \u0f50
+    | Cd -- ད \u0f51
+    | CdPLUSh -- དྷ \u0f52
+    | Cn -- ན \u0f53
+    | Cp -- པ \u0f54
+    | Cph -- ཕ \u0f55
+    | Cb -- བ \u0f56
+    | CbPLUSh -- བྷ \u0f57
+    | Cm -- མ \u0f58
+    | Cts -- ཙ \u0f59
+    | Ctsh -- ཚ \u0f5a
+    | Cdz -- ཛ \u0f5b
+    | CdzPLUSh -- ཛྷ \u0f5c
+    | Cw -- ཝ \u0f5d
+    | Czh -- ཞ \u0f5e
+    | Cz -- ཟ \u0f5f
+    | C' -- འ \u0f60
+    | Cy -- ཡ \u0f61
+    | Cr -- ར \u0f62
+    | Cl -- ལ \u0f63
+    | Csh -- ཤ \u0f64
+    | CSh -- ཥ \u0f65
+    | Cs -- ས \u0f66
+    | Ch -- ཧ \u0f67
+    | Ca -- ཨ \u0f68
+    | CkPLUSSh -- ཀྵ \u0f69
+    | CR -- ཪ \u0f6a
     deriving (Show, Eq)
 
 data Vowel
     = VA -- ཱ \u0f71
-    | Vi   -- ི \u0f72 
-    | VI   -- ཱི \u0f73
-    | Vu   -- ུ \u0f74
-    | VU     -- ཱུ \u0f75
+    | Vi -- ི \u0f72
+    | VI -- ཱི \u0f73
+    | Vu -- ུ \u0f74
+    | VU -- ཱུ \u0f75
     | Vr_i -- ྲྀ \u0f76
-    | Vr_I   -- ཷ \u0f77
+    | Vr_I -- ཷ \u0f77
     | Vl_i -- ླྀ \u0f78
-    | Vl_I   -- ཹ \u0f79
-    | Ve  -- ེ \u0f7a
+    | Vl_I -- ཹ \u0f79
+    | Ve -- ེ \u0f7a
     | Vai -- ཻ \u0f7b
     | Vo -- ོ \u0f7c
     | Vau -- ཽ \u0f7d
-    | V_i -- ྀ \u0f80 
+    | V_i -- ྀ \u0f80
     | V_I -- ཱྀ \u0f81
 
-    -- | Vuo -- "ོུ"
-    -- | Vui -- "ིུ" 
-    -- | Vue -- "ེུ"
+    -- | Vuo -- ོུ \u0f74\u0f7c
+    -- | Vui -- ིུ \u0f74\u0f72
+    -- | Vue -- ེུ \u0f74\u0f7a
     deriving (Show, Eq)
 
 data Number
-    = N0 -- ༠
-    | N1 -- ༡
-    | N2 -- ༢
-    | N3 -- ༣
-    | N4 -- ༤
-    | N5 -- ༥
-    | N6 -- ༦
-    | N7 -- ༧
-    | N8 -- ༨
-    | N9 -- ༩
+    = N0 -- ༠ \u0f20
+    | N1 -- ༡ \u0f21
+    | N2 -- ༢ \u0f22
+    | N3 -- ༣ \u0f23
+    | N4 -- ༤ \u0f24
+    | N5 -- ༥ \u0f25
+    | N6 -- ༦ \u0f26
+    | N7 -- ༧ \u0f27
+    | N8 -- ༨ \u0f28
+    | N9 -- ༩ \u0f29
     deriving (Show, Eq)
 
 data HalfNumber
-    = H_0 -- ༳
-    | H_1 -- ༪
-    | H_2 -- ༫
-    | H_3 -- ༬
-    | H_4 -- ༭
-    | H_5 -- ༮
-    | H_6 -- ༯
-    | H_7 -- ༰
-    | H_8 -- ༱
-    | H_9 -- ༲
+    = H_0 -- ༳ \u0f33
+    | H_1 -- ༪ \u0f2a
+    | H_2 -- ༫ \u0f2b
+    | H_3 -- ༬ \u0f2c
+    | H_4 -- ༭ \u0f2d
+    | H_5 -- ༮ \u0f2e
+    | H_6 -- ༯ \u0f2f
+    | H_7 -- ༰ \u0f30
+    | H_8 -- ༱ \u0f31
+    | H_9 -- ༲ \u0f32
+    deriving (Show, Eq)
+
+-- Subjoined Tibetan consonants used in stacks.
+data SubConsonant
+    = SCk -- ྐ \u0f90
+    | SCkh -- ྑ \u0f91
+    | SCg -- ྒ \u0f92
+    | SCgPLUSh -- ྒྷ \u0f93
+    | SCng -- ྔ \u0f94
+    | SCc -- ྕ \u0f95
+    | SCch -- ྖ \u0f96
+    | SCj -- ྗ \u0f97
+    | SCny -- ྙ \u0f99
+    | SCT -- ྚ \u0f9a
+    | SCTh -- ྛ \u0f9b
+    | SCD -- ྜ \u0f9c
+    | SCDPLUSh -- ྜྷ \u0f9d
+    | SCN -- ྞ \u0f9e
+    | SCt -- ྟ \u0f9f
+    | SCth -- ྠ \u0fa0
+    | SCd -- ྡ \u0fa1
+    | SCdPLUSh -- ྡྷ \u0fa2
+    | SCn -- ྣ \u0fa3
+    | SCp -- ྤ \u0fa4
+    | SCph -- ྥ \u0fa5
+    | SCb -- ྦ \u0fa6
+    | SCbPLUSh -- ྦྷ \u0fa7
+    | SCm -- ྨ \u0fa8
+    | SCts -- ྩ \u0fa9
+    | SCtsh -- ྪ \u0faa
+    | SCdz -- ྫ \u0fab
+    | SCdzPLUSh -- ྫྷ \u0fac
+    | SCw -- ྭ \u0fad
+    | SCzh -- ྮ \u0fae
+    | SCz -- ྯ \u0faf
+    | SC' -- ྰ \u0fb0
+    | SCy -- ྱ \u0fb1
+    | SCr -- ྲ \u0fb2
+    | SCl -- ླ \u0fb3
+    | SCsh -- ྴ \u0fb4
+    | SCSh -- ྵ \u0fb5
+    | SCs -- ྶ \u0fb6
+    | SCh -- ྷ \u0fb7
+    | SCa -- ྸ \u0fb8
+    | SCkPLUSSh -- ྐྵ \u0fb9
+    | SCW -- ྺ \u0fba
+    | SCY -- ྻ \u0fbb
+    | SCR -- ྼ \u0fbc
+    deriving (Show, Eq)
+
+data FinalMark
+    = FMAnusvara -- ཾ \u0f7e, ྂ \u0f82, ྃ \u0f83
+    | FMVisarga -- ཿ \u0f7f
+    | FMCandrabinduOrNasal -- ༵ \u0f35, ༷ \u0f37
+    | FMHalanta -- ྄ \u0f84
+    | FMCaret -- ྐྵ \u0f39
+    | FMYigMgo -- ྅ \u0f85
+    deriving (Show, Eq)
+
+data PunctuationMark
+    = PMTsheg -- ་ \u0f0b
+    | PMNonBreakingTsheg -- ༌ \u0f0c
+    | PMShad -- ། \u0f0d
+    | PMNyisShad -- ༎ \u0f0e
+    | PMTshegShad -- ༏ \u0f0f
+    | PMRinChenSpungsShad -- ༑ \u0f11
+    | PMGterTshigMgo -- ༔ \u0f14
+    deriving (Show, Eq)
+
+data SpaceMark
+    = SMSpace --   \u0020
+    deriving (Show, Eq)
+
+data SymbolMark
+    = SMExclamation -- ! \u0021
+    | SMAt -- @ \u0040
+    | SMHash -- # \u0023
+    | SMDollar -- $ \u0024
+    | SMPercent -- % \u0025
+    | SMEqual -- = \u003d
+    | SMLt -- < \u003c
+    | SMGt -- > \u003e
+    | SMLParen -- ( \u0028
+    | SMRParen -- ) \u0029
+    | SMAsterisk -- * \u002a
+    | SMSlash -- / \u002f
+    | SMDoubleSlash -- // \u002f\u002f
+    | SMSemicolon -- ; \u003b
+    | SMBar -- | \u007c
+    | SMColon -- : \u003a
+    deriving (Show, Eq)
+
+newtype UnknownMark = UnknownMark Text
     deriving (Show, Eq)
 
 -- >>> import Data.Char
