c03248c264d047857d0e4fcc52fa6b4b38c5facf
Vladislav Sabanov <email@email.com>
2026-09-07 15:03:45 +0500

    Add more span tests

--- hewts/test/Test/Tokenizer/Spans.hs
+++ hewts/test/Test/Tokenizer/Spans.hs
@@ -6,7 +6,7 @@ import Convert.Tokenizer.Wylie (tokenizeWylie)
 import qualified Data.Text as T
 import Numeric.Natural (Natural)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ((@?=), Assertion, testCase)
+import Test.Tasty.HUnit ((@?=), Assertion, assertBool, testCase)
 
 tests :: TestTree
 tests =
@@ -16,6 +16,10 @@ tests =
         , testCase "wylie multi-char token span length" caseWylieMultiCharLen
         , testCase "unicode spans are contiguous" (assertContiguousSpans $ tokenizeUnicode "ཚ །།")
         , testCase "unicode final token ends at input length" caseUnicodeEndsAtLength
+        , testCase "wylie stream invariants on mixed input" caseWylieStreamInvariants
+        , testCase "unicode stream invariants on mixed input" caseUnicodeStreamInvariants
+        , testCase "wylie stream invariants on empty input" (assertTokenStreamInvariants tokenizeWylie "")
+        , testCase "unicode stream invariants on empty input" (assertTokenStreamInvariants tokenizeUnicode "")
         ]
 
 caseWylieMultiCharLen :: Assertion
@@ -33,6 +37,14 @@ caseUnicodeEndsAtLength =
             [] -> error "Expected non-empty token stream"
             tok : _ -> offsetEnd (tokenSpan tok) @?= expectedEnd
 
+caseWylieStreamInvariants :: Assertion
+caseWylieStreamInvariants =
+    assertTokenStreamInvariants tokenizeWylie "gzhon // ~+`]-. x _ k+Sh"
+
+caseUnicodeStreamInvariants :: Assertion
+caseUnicodeStreamInvariants =
+    assertTokenStreamInvariants tokenizeUnicode "ཀིི ཀxི ྆། ་"
+
 assertContiguousSpans :: [Token] -> Assertion
 assertContiguousSpans [] = pure ()
 assertContiguousSpans toks = go 0 toks
@@ -44,3 +56,16 @@ assertContiguousSpans toks = go 0 toks
         let end = offsetEnd (tokenSpan tok)
         end @?= expectedStart + fromIntegral (T.length (tokenRaw tok))
         go end rest
+
+assertTokenStreamInvariants :: (T.Text -> [Token]) -> T.Text -> Assertion
+assertTokenStreamInvariants tokenizer input = do
+    let toks = tokenizer input
+    assertBool "Token stream contains empty tokenRaw" (all (not . T.null . tokenRaw) toks)
+    T.concat (map tokenRaw toks) @?= input
+    mapM_ assertUnknownCanonicalEqualsRaw toks
+
+assertUnknownCanonicalEqualsRaw :: Token -> Assertion
+assertUnknownCanonicalEqualsRaw tok =
+    case tokenKind tok of
+        TkUnknown -> tokenCanonical tok @?= TcUnknown (UnknownMark (tokenRaw tok))
+        _ -> pure ()
