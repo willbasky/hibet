051bbf1eab661e8f3f8985e2faeb87cd0f918f58
Vladislav Sabanov <email@email.com>
2026-09-08 22:32:43 +0500

    Add rest structures and tests

--- hewts/src/Convert/Grammar/Rule.hs
+++ hewts/src/Convert/Grammar/Rule.hs
@@ -15,9 +15,39 @@ module Convert.Grammar.Rule
     , pStructure6
     , pStructure7
     , pStructure8
+    , pStructure9
+    , pStructure10
+    , pStructure11
+    , pStructure12
+    , pStructure13
+    , pStructure14
+    , pStructure15
+    , pStructure16
+    , pStructure17
+    , pStructure18
+    , pStructure19
+    , pStructure20
+    , pStructure21
+    , pStructure22
+    , pStructure23
+    , pStructure24
+    , pStructure25
+    , pStructure26
+    , pStructure27
+    , pStructure28
+    , pStructure29
+    , pStructure30
+    , pStructure31
+    , pStructure32
+    , pStructure33
+    , pStructure34
+    , pStructure35
+    , pStructure36
+    , pStructure37
     ) where
 
 import Convert.Grammar.Parser (Parser)
+import qualified Convert.Grammar.Parser as GP
 import qualified Convert.Grammar.Rule.Constraint01 as C01
 import qualified Convert.Grammar.Rule.Constraint08 as C08
 import qualified Convert.Grammar.Rule.Constraint09 as C09
@@ -26,7 +56,14 @@ import qualified Convert.Grammar.Rule.Constraint11 as C11
 import qualified Convert.Grammar.Rule.Constraint12 as C12
 import qualified Convert.Grammar.Rule.Constraint13 as C13
 import qualified Convert.Grammar.Rule.Constraint14 as C14
+import qualified Convert.Grammar.Rule.Constraint15 as C15
+import qualified Convert.Grammar.Rule.Constraint16 as C16
+import qualified Convert.Grammar.Rule.Constraint17 as C17
+import qualified Convert.Grammar.Rule.Constraint18 as C18
+import qualified Convert.Grammar.Rule.Constraint19 as C19
+import qualified Convert.Grammar.Rule.Constraint20 as C20
 import Convert.Token (Token)
+import Data.Maybe (maybeToList)
 import qualified Text.Megaparsec as MP
 
 -- Tibetan spelling structure 1
@@ -71,3 +108,208 @@ pStructure7 = C13.pConstraint13
 -- On the basis of the Tibetan spelling grammar 4.14
 pStructure8 :: Parser [Token]
 pStructure8 = C14.pConstraint14
+
+-- Tibetan spelling structure 9
+-- On the basis of the Tibetan spelling grammar 4.14 and 4.15
+pStructure9 :: Parser [Token]
+pStructure9 = C14.pConstraint14 <> C15.pConstraint15
+
+-- Tibetan spelling structure 10
+-- On the basis of the Tibetan spelling grammar 4.11 and 4.15
+pStructure10 :: Parser [Token]
+pStructure10 = C11.pConstraint11 <> C15.pConstraint15
+
+-- Tibetan spelling structure 11
+-- On the basis of the Tibetan spelling grammar 4.12 and 4.15
+pStructure11 :: Parser [Token]
+pStructure11 = C12.pConstraint12 <> C15.pConstraint15
+
+-- Tibetan spelling structure 12
+-- On the basis of the Tibetan spelling grammar 4.13 and 4.15
+pStructure12 :: Parser [Token]
+pStructure12 = C13.pConstraint13 <> C15.pConstraint15
+
+-- Tibetan spelling structure 13
+-- On the basis of the Tibetan spelling grammar 4.14, 4.15, 4.16
+pStructure13 :: Parser [Token]
+pStructure13 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C14.pConstraint14 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C14.pConstraint14 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 14
+-- On the basis of the Tibetan spelling grammar 4.11, 4.15, 4.16
+pStructure14 :: Parser [Token]
+pStructure14 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C11.pConstraint11 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C11.pConstraint11 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 15
+-- On the basis of the Tibetan spelling grammar 4.12, 4.15, 4.16
+pStructure15 :: Parser [Token]
+pStructure15 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C12.pConstraint12 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C12.pConstraint12 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 16
+-- On the basis of the Tibetan spelling grammar 4.13, 4.15, 4.16
+pStructure16 :: Parser [Token]
+pStructure16 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C13.pConstraint13 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C13.pConstraint13 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+parseSuffixPostfix :: Parser [Token] -> Parser [Token] -> Parser Token -> Parser [Token]
+parseSuffixPostfix base suffix post = do
+    b <- base
+    s <- suffix
+    p <- post
+    pure (b <> s <> [p])
+
+-- Tibetan spelling structure 17
+-- On the basis of the Tibetan spelling grammar 4.15
+pStructure17 :: Parser [Token]
+pStructure17 = do
+    root <- GP.pRootConsonant
+    vowel <- MP.optional GP.pVowel
+    suffix <- C15.pConstraint15
+    pure (root : maybeToList vowel <> suffix)
+
+-- Tibetan spelling structure 18
+-- On the basis of the Tibetan spelling grammar 4.8 and 4.15
+pStructure18 :: Parser [Token]
+pStructure18 = C08.pConstraint08 <> C15.pConstraint15
+
+-- Tibetan spelling structure 19
+-- On the basis of the Tibetan spelling grammar 4.9 and 4.15
+pStructure19 :: Parser [Token]
+pStructure19 = C09.pConstraint09 <> C15.pConstraint15
+
+-- Tibetan spelling structure 20
+-- On the basis of the Tibetan spelling grammar 4.10 and 4.15
+pStructure20 :: Parser [Token]
+pStructure20 = C10.pConstraint10 <> C15.pConstraint15
+
+-- Tibetan spelling structure 21
+-- On the basis of the Tibetan spelling grammar 4.1, 4.14, 4.15
+pStructure21 :: Parser [Token]
+pStructure21 =
+    MP.choice
+        [ MP.try $ rootSuffixPostfix C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ rootSuffixPostfix C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 22
+-- On the basis of the Tibetan spelling grammar 4.8, 4.14, 4.15
+pStructure22 :: Parser [Token]
+pStructure22 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C08.pConstraint08 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C08.pConstraint08 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 23
+-- On the basis of the Tibetan spelling grammar 4.9, 4.14, 4.15
+pStructure23 :: Parser [Token]
+pStructure23 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C09.pConstraint09 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C09.pConstraint09 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+-- Tibetan spelling structure 24
+-- On the basis of the Tibetan spelling grammar 4.10, 4.14, 4.15
+pStructure24 :: Parser [Token]
+pStructure24 =
+    MP.choice
+        [ MP.try $ parseSuffixPostfix C10.pConstraint10 C16.pConstraint16Da GP.pPostfixDa
+        , MP.try $ parseSuffixPostfix C10.pConstraint10 C16.pConstraint16Sa GP.pPostfixSa
+        ]
+
+rootSuffixPostfix :: Parser [Token] -> Parser Token -> Parser [Token]
+rootSuffixPostfix suffix post = do
+    root <- GP.pRootConsonant
+    vowel <- MP.optional GP.pVowel
+    s <- suffix
+    p <- post
+    pure (root : maybeToList vowel <> s <> [p])
+
+-- Tibetan spelling structure 25
+-- On the basis of the Tibetan spelling grammar 4.17
+pStructure25 :: Parser [Token]
+pStructure25 =
+    MP.choice
+        [ MP.try C17.pConstraint17Ra
+        , MP.try C17.pConstraint17Ya
+        ]
+
+-- Tibetan spelling structure 26
+-- On the basis of the Tibetan spelling grammar 4.18
+pStructure26 :: Parser [Token]
+pStructure26 = C18.pConstraint18
+
+-- Tibetan spelling structure 27
+-- On the basis of the Tibetan spelling grammar 4.19
+pStructure27 :: Parser [Token]
+pStructure27 = C19.pConstraint19
+
+-- Tibetan spelling structure 28
+-- On the basis of the Tibetan spelling grammar 4.1 and 4.20
+pStructure28 :: Parser [Token]
+pStructure28 = C01.pConstraint01 <> C20.pConstraint20
+
+-- Tibetan spelling structure 29
+-- On the basis of the Tibetan spelling grammar 4.8 and 4.20
+pStructure29 :: Parser [Token]
+pStructure29 = C08.pConstraint08 <> C20.pConstraint20
+
+-- Tibetan spelling structure 30
+-- On the basis of the Tibetan spelling grammar 4.9 and 4.20
+pStructure30 :: Parser [Token]
+pStructure30 = C09.pConstraint09 <> C20.pConstraint20
+
+-- Tibetan spelling structure 31
+-- On the basis of the Tibetan spelling grammar 4.10 and 4.20
+pStructure31 :: Parser [Token]
+pStructure31 = C10.pConstraint10 <> C20.pConstraint20
+
+-- Tibetan spelling structure 32
+-- On the basis of the Tibetan spelling grammar 4.11 and 4.20
+pStructure32 :: Parser [Token]
+pStructure32 = C11.pConstraint11 <> C20.pConstraint20
+
+-- Tibetan spelling structure 33
+-- On the basis of the Tibetan spelling grammar 4.12 and 4.20
+pStructure33 :: Parser [Token]
+pStructure33 = C12.pConstraint12 <> C20.pConstraint20
+
+-- Tibetan spelling structure 34
+-- On the basis of the Tibetan spelling grammar 4.13 and 4.20
+pStructure34 :: Parser [Token]
+pStructure34 = C13.pConstraint13 <> C20.pConstraint20
+
+-- Tibetan spelling structure 35
+-- On the basis of the Tibetan spelling grammar 4.14 and 4.20
+pStructure35 :: Parser [Token]
+pStructure35 = C14.pConstraint14 <> C20.pConstraint20
+
+-- Tibetan spelling structure 36
+-- On the basis of the Tibetan spelling grammar 4.17 and 4.20
+pStructure36 :: Parser [Token]
+pStructure36 =
+    (MP.choice
+        [ MP.try C17.pConstraint17Ra
+        , MP.try C17.pConstraint17Ya
+        ])
+        <> C20.pConstraint20
+
+-- Tibetan spelling structure 37
+-- On the basis of the Tibetan spelling grammar 4.18 and 4.20
+pStructure37 :: Parser [Token]
+pStructure37 = C18.pConstraint18 <> C20.pConstraint20
--- hewts/test/Test/Grammar/Rule/Structures.hs
+++ hewts/test/Test/Grammar/Rule/Structures.hs
@@ -10,6 +10,35 @@ import Convert.Grammar.Rule
   , pStructure6
   , pStructure7
   , pStructure8
+  , pStructure9
+  , pStructure10
+  , pStructure11
+  , pStructure12
+  , pStructure13
+  , pStructure14
+  , pStructure15
+  , pStructure16
+  , pStructure17
+  , pStructure18
+  , pStructure19
+  , pStructure20
+  , pStructure21
+  , pStructure22
+  , pStructure23
+  , pStructure24
+  , pStructure25
+  , pStructure26
+  , pStructure27
+  , pStructure28
+  , pStructure29
+  , pStructure30
+  , pStructure31
+  , pStructure32
+  , pStructure33
+  , pStructure34
+  , pStructure35
+  , pStructure36
+  , pStructure37
   )
 import Convert.Token (Token, tokenRaw)
 import Convert.Tokenizer.Unicode (tokenizeUnicode)
@@ -30,6 +59,35 @@ tests =
     , structure6
     , structure7
     , structure8
+    , structure9
+    , structure10
+    , structure11
+    , structure12
+    , structure13
+    , structure14
+    , structure15
+    , structure16
+    , structure17
+    , structure18
+    , structure19
+    , structure20
+    , structure21
+    , structure22
+    , structure23
+    , structure24
+    , structure25
+    , structure26
+    , structure27
+    , structure28
+    , structure29
+    , structure30
+    , structure31
+    , structure32
+    , structure33
+    , structure34
+    , structure35
+    , structure36
+    , structure37
     ]
 
 structure1 :: TestTree
@@ -138,6 +196,362 @@ structure8 =
         parseRaws pStructure8 "གཏ" @?= Right ["ག", "ཏ"]
     , testCase "rejects root ང above prefix བ" $
         isLeft (parseRaws pStructure8 "བང") @?= True
+    ]
+
+structure9 :: TestTree
+structure9 =
+  testGroup
+    "structure 9"
+    [ testCase "བཏག" $
+        parseRaws pStructure9 "བཏག" @?= Right ["བ", "ཏ", "ག"]
+    , testCase "བཏིག" $
+        parseRaws pStructure9 "བཏིག" @?= Right ["བ", "ཏ", "ི", "ག"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure9 "བཏ") @?= True
+    ]
+
+structure10 :: TestTree
+structure10 =
+  testGroup
+    "structure 10"
+    [ testCase "བརྒན" $
+        parseRaws pStructure10 "བརྒན" @?= Right ["བ", "ར", "ྒ", "ན"]
+    , testCase "བརྒིག" $
+        parseRaws pStructure10 "བརྒིག" @?= Right ["བ", "ར", "ྒ", "ི", "ག"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure10 "བརྒ") @?= True
+    ]
+
+structure11 :: TestTree
+structure11 =
+  testGroup
+    "structure 11"
+    [ testCase "མགྱས" $
+        parseRaws pStructure11 "མགྱས" @?= Right ["མ", "ག", "ྱ", "ས"]
+    , testCase "མགྲིས" $
+        parseRaws pStructure11 "མགྲིས" @?= Right ["མ", "ག", "ྲ", "ི", "ས"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure11 "མགྲ") @?= True
+    ]
+
+structure12 :: TestTree
+structure12 =
+  testGroup
+    "structure 12"
+    [ testCase "བརྒྱས" $
+        parseRaws pStructure12 "བརྒྱས" @?= Right ["བ", "ར", "ྒ", "ྱ", "ས"]
+    , testCase "བསྒྲས" $
+        parseRaws pStructure12 "བསྒྲས" @?= Right ["བ", "ས", "ྒ", "ྲ", "ས"]
+    , testCase "བསྒྲོས" $
+        parseRaws pStructure12 "བསྒྲོས" @?= Right ["བ", "ས", "ྒ", "ྲ", "ོ", "ས"]
+    , testCase "བསྒྱས" $
+        parseRaws pStructure12 "བསྒྱས" @?= Right ["བ", "ས", "ྒ", "ྱ", "ས"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure12 "བསྒྲ") @?= True
+    ]
+
+structure13 :: TestTree
+structure13 =
+  testGroup
+    "structure 13"
+    [ testCase "བཏནད" $
+        parseRaws pStructure13 "བཏནད" @?= Right ["བ", "ཏ", "ན", "ད"]
+    , testCase "གཏིགས" $
+        parseRaws pStructure13 "གཏིགས" @?= Right ["ག", "ཏ", "ི", "ག", "ས"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure13 "བཏན") @?= True
+    ]
+
+structure14 :: TestTree
+structure14 =
+  testGroup
+    "structure 14"
+    [ testCase "བརྟགས" $
+        parseRaws pStructure14 "བརྟགས" @?= Right ["བ", "ར", "ྟ", "ག", "ས"]
+    , testCase "བརྟིབས" $
+        parseRaws pStructure14 "བརྟིབས" @?= Right ["བ", "ར", "ྟ", "ི", "བ", "ས"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure14 "བརྟག") @?= True
+    ]
+
+structure15 :: TestTree
+structure15 =
+  testGroup
+    "structure 15"
+    [ testCase "དགྱིགས" $
+        parseRaws pStructure15 "དགྱིགས" @?= Right ["ད", "ག", "ྱ", "ི", "ག", "ས"]
+    , testCase "དགྲོགས" $
+        parseRaws pStructure15 "དགྲོགས" @?= Right ["ད", "ག", "ྲ", "ོ", "ག", "ས"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure15 "དགྱིག") @?= True
+    ]
+
+structure16 :: TestTree
+structure16 =
+  testGroup
+    "structure 16"
+    [ testCase "བསྒྱུགས" $
+        parseRaws pStructure16 "བསྒྱུགས" @?= Right ["བ", "ས", "ྒ", "ྱ", "ུ", "ག", "ས"]
+    , testCase "བརྐྱནད" $
+        parseRaws pStructure16 "བརྐྱནད" @?= Right ["བ", "ར", "ྐ", "ྱ", "ན", "ད"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure16 "བསྒྱུག") @?= True
+    ]
+
+structure17 :: TestTree
+structure17 =
+  testGroup
+    "structure 17"
+    [ testCase "བིག" $
+        parseRaws pStructure17 "བིག" @?= Right ["བ", "ི", "ག"]
+    , testCase "བས" $
+        parseRaws pStructure17 "བས" @?= Right ["བ", "ས"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure17 "བི") @?= True
+    ]
+
+structure18 :: TestTree
+structure18 =
+  testGroup
+    "structure 18"
+    [ testCase "རྒིད" $
+        parseRaws pStructure18 "རྒིད" @?= Right ["ར", "ྒ", "ི", "ད"]
+    , testCase "སྒོལ" $
+        parseRaws pStructure18 "སྒོལ" @?= Right ["ས", "ྒ", "ོ", "ལ"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure18 "སྒོ") @?= True
+    ]
+
+structure19 :: TestTree
+structure19 =
+  testGroup
+    "structure 19"
+    [ testCase "ཁྱོའ" $
+        parseRaws pStructure19 "ཁྱོའ" @?= Right ["ཁ", "ྱ", "ོ", "འ"]
+    , testCase "བླིན" $
+        parseRaws pStructure19 "བླིན" @?= Right ["བ", "ླ", "ི", "ན"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure19 "ཁྱོ") @?= True
+    ]
+
+structure20 :: TestTree
+structure20 =
+  testGroup
+    "structure 20"
+    [ testCase "སྐྲོན" $
+        parseRaws pStructure20 "སྐྲོན" @?= Right ["ས", "ྐ", "ྲ", "ོ", "ན"]
+    , testCase "སྒྱར" $
+        parseRaws pStructure20 "སྒྱར" @?= Right ["ས", "ྒ", "ྱ", "ར"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure20 "སྐྲོ") @?= True
+    ]
+
+structure21 :: TestTree
+structure21 =
+  testGroup
+    "structure 21"
+    [ testCase "བགས" $
+        parseRaws pStructure21 "བགས" @?= Right ["བ", "ག", "ས"]
+    , testCase "པོགས" $
+        parseRaws pStructure21 "པོགས" @?= Right ["པ", "ོ", "ག", "ས"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure21 "པོག") @?= True
+    ]
+
+structure22 :: TestTree
+structure22 =
+  testGroup
+    "structure 22"
+    [ testCase "རྨགས" $
+        parseRaws pStructure22 "རྨགས" @?= Right ["ར", "ྨ", "ག", "ས"]
+    , testCase "སྣོབས" $
+        parseRaws pStructure22 "སྣོབས" @?= Right ["ས", "ྣ", "ོ", "བ", "ས"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure22 "རྨག") @?= True
+    ]
+
+structure23 :: TestTree
+structure23 =
+  testGroup
+    "structure 23"
+    [ testCase "རློམས" $
+        parseRaws pStructure23 "རློམས" @?= Right ["ར", "ླ", "ོ", "མ", "ས"]
+    , testCase "དྭོལད" $
+        parseRaws pStructure23 "དྭོལད" @?= Right ["ད", "ྭ", "ོ", "ལ", "ད"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure23 "རློམ") @?= True
+    ]
+
+structure24 :: TestTree
+structure24 =
+  testGroup
+    "structure 24"
+    [ testCase "རྩྭོངས" $
+        parseRaws pStructure24 "རྩྭོངས" @?= Right ["ར", "ྩ", "ྭ", "ོ", "ང", "ས"]
+    , testCase "སྣྲེནད" $
+        parseRaws pStructure24 "སྣྲེནད" @?= Right ["ས", "ྣ", "ྲ", "ེ", "ན", "ད"]
+    , testCase "rejects missing postfix" $
+        isLeft (parseRaws pStructure24 "རྩྭོང") @?= True
+    ]
+
+structure25 :: TestTree
+structure25 =
+  testGroup
+    "structure 25"
+    [ testCase "དྲྭ" $
+        parseRaws pStructure25 "དྲྭ" @?= Right ["ད", "ྲ", "ྭ"]
+    , testCase "ཕྱྭ" $
+        parseRaws pStructure25 "ཕྱྭ" @?= Right ["ཕ", "ྱ", "ྭ"]
+    , testCase "rejects subfix without ra/ya root" $
+        isLeft (parseRaws pStructure25 "དྭ") @?= True
+    ]
+
+structure26 :: TestTree
+structure26 =
+  testGroup
+    "structure 26"
+    [ testCase "ཧྥ" $
+        parseRaws pStructure26 "ཧྥ" @?= Right ["ཧ", "ྥ"]
+    , testCase "ཧྥོ" $
+        parseRaws pStructure26 "ཧྥོ" @?= Right ["ཧ", "ྥ", "ོ"]
+    , testCase "rejects wrong subroot" $
+        isLeft (parseRaws pStructure26 "ཧྲ") @?= True
+    ]
+
+structure27 :: TestTree
+structure27 =
+  testGroup
+    "structure 27"
+    [ testCase "ཧྥེལ" $
+        parseRaws pStructure27 "ཧྥེལ" @?= Right ["ཧ", "ྥ", "ེ", "ལ"]
+    , testCase "ཧྥོས" $
+        parseRaws pStructure27 "ཧྥོས" @?= Right ["ཧ", "ྥ", "ོ", "ས"]
+    , testCase "rejects missing suffix" $
+        isLeft (parseRaws pStructure27 "ཧྥ") @?= True
+    ]
+
+structure28 :: TestTree
+structure28 =
+  testGroup
+    "structure 28"
+    [ testCase "ཧིའེ" $
+        parseRaws pStructure28 "ཧིའེ" @?= Right ["ཧ", "ི", "འ", "ེ"]
+    , testCase "གོའྨ" $
+        parseRaws pStructure28 "གོའྨ" @?= Right ["ག", "ོ", "འ", "ྨ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure28 "ཧི") @?= True
+    ]
+
+structure29 :: TestTree
+structure29 =
+  testGroup
+    "structure 29"
+    [ testCase "སྨོའྨ" $
+        parseRaws pStructure29 "སྨོའྨ" @?= Right ["ས", "ྨ", "ོ", "འ", "ྨ"]
+    , testCase "རྒའོ" $
+        parseRaws pStructure29 "རྒའོ" @?= Right ["ར", "ྒ", "འ", "ོ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure29 "སྨོ") @?= True
+    ]
+
+structure30 :: TestTree
+structure30 =
+  testGroup
+    "structure 30"
+    [ testCase "སླིའྔ" $
+        parseRaws pStructure30 "སླིའྔ" @?= Right ["ས", "ླ", "ི", "འ", "ྔ"]
+    , testCase "ཀྭོའོ" $
+        parseRaws pStructure30 "ཀྭོའོ" @?= Right ["ཀ", "ྭ", "ོ", "འ", "ོ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure30 "སླི") @?= True
+    ]
+
+structure31 :: TestTree
+structure31 =
+  testGroup
+    "structure 31"
+    [ testCase "སྒྱའོ" $
+        parseRaws pStructure31 "སྒྱའོ" @?= Right ["ས", "ྒ", "ྱ", "འ", "ོ"]
+    , testCase "རྐྱའོ" $
+        parseRaws pStructure31 "རྐྱའོ" @?= Right ["ར", "ྐ", "ྱ", "འ", "ོ"]
+    , testCase "རྐྱའ" $
+        parseRaws pStructure31 "རྐྱའ" @?= Right ["ར", "ྐ", "ྱ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure31 "སྒྱ") @?= True
+    ]
+
+structure32 :: TestTree
+structure32 =
+  testGroup
+    "structure 32"
+    [ testCase "བལྟའ" $
+        parseRaws pStructure32 "བལྟའ" @?= Right ["བ", "ལ", "ྟ", "འ"]
+    , testCase "བརྔོའ" $
+        parseRaws pStructure32 "བརྔོའ" @?= Right ["བ", "ར", "ྔ", "ོ", "འ"]
+    , testCase "བསྟིའི" $
+        parseRaws pStructure32 "བསྟིའི" @?= Right ["བ", "ས", "ྟ", "ི", "འ", "ི"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure32 "བལྟ") @?= True
+    ]
+
+structure33 :: TestTree
+structure33 =
+  testGroup
+    "structure 33"
+    [ testCase "མཁྲའ" $
+        parseRaws pStructure33 "མཁྲའ" @?= Right ["མ", "ཁ", "ྲ", "འ"]
+    , testCase "དཔྱུའ" $
+        parseRaws pStructure33 "དཔྱུའ" @?= Right ["ད", "པ", "ྱ", "ུ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure33 "མཁྲ") @?= True
+    ]
+
+structure34 :: TestTree
+structure34 =
+  testGroup
+    "structure 34"
+    [ testCase "བསྐྲའ" $
+        parseRaws pStructure34 "བསྐྲའ" @?= Right ["བ", "ས", "ྐ", "ྲ", "འ"]
+    , testCase "བརྒྱའ" $
+        parseRaws pStructure34 "བརྒྱའ" @?= Right ["བ", "ར", "ྒ", "ྱ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure34 "བསྐྲ") @?= True
+    ]
+
+structure35 :: TestTree
+structure35 =
+  testGroup
+    "structure 35"
+    [ testCase "བཟིའ" $
+        parseRaws pStructure35 "བཟིའ" @?= Right ["བ", "ཟ", "ི", "འ"]
+    , testCase "གཤའ" $
+        parseRaws pStructure35 "གཤའ" @?= Right ["ག", "ཤ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure35 "བཟ") @?= True
+    ]
+
+structure36 :: TestTree
+structure36 =
+  testGroup
+    "structure 36"
+    [ testCase "གྲྭའ" $
+        parseRaws pStructure36 "གྲྭའ" @?= Right ["ག", "ྲ", "ྭ", "འ"]
+    , testCase "ཕྱྭུའ" $
+        parseRaws pStructure36 "ཕྱྭུའ" @?= Right ["ཕ", "ྱ", "ྭ", "ུ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure36 "གྲྭ") @?= True
+    ]
+
+structure37 :: TestTree
+structure37 =
+  testGroup
+    "structure 37"
+    [ testCase "ཧྥོའ" $
+        parseRaws pStructure37 "ཧྥོའ" @?= Right ["ཧ", "ྥ", "ོ", "འ"]
+    , testCase "ཧྥའ" $
+        parseRaws pStructure37 "ཧྥའ" @?= Right ["ཧ", "ྥ", "འ"]
+    , testCase "rejects missing a-chung chain" $
+        isLeft (parseRaws pStructure37 "ཧྥ") @?= True
     ]
 
 parseRaws :: Parser [Token] -> Text -> Either Text [Text]
